fcdb203dd60d2ecb40e3efc42dbb05ac00f7c27e
bart412 <bartekg412@gmail.com>
2022-03-17 13:17:38 +0100

    Created model

--- budget_app/admin.py
+++ budget_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
+from .models import Category, BudgetUser, Budget, ExpenseIncome
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(BudgetUser)
+admin.site.register(Budget)
+admin.site.register(ExpenseIncome)
--- budget_app/models.py
+++ budget_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
 
 # Create your models here.
+import budget_app.models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=150)
+    parent_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Budget(models.Model):
+    name = models.CharField(max_length=30)
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField()
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='BudgetUser')
+
+
+class BudgetUser(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    budget_id = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    role = models.CharField(max_length=30)
+
+
+class ExpenseIncome(models.Model):
+    name = models.CharField(max_length=30)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=150)
+    date = models.DateField(default=date.today)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    budget_id = models.ForeignKey(Budget, on_delete=models.CASCADE)
