91d694f68920645c6eb89a3fcb5425b0f47164f0
bart412 <bartekg412@gmail.com>
2022-03-23 21:17:12 +0100

    repaired /login

--- budget_app/views.py
+++ budget_app/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def base(request):
-    return render(request, 'budget_app/base.html')
+    return render(request, "budget_app/base.html")
+
+
 def home(request):
-    return render(request, 'budget_app/home.html')
+    return render(request, "budget_app/home.html")
+
+
 def register(request):
-    return render(request, 'budget_app/register.html')
+    return render(request, "budget_app/register.html")
