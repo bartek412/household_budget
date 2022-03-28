614cea42123cfe6e8bc1f2dce24b4d325f1eb77a
Kinga Stawczyk <kingastawczyk@gmail.com>
2022-03-28 22:30:27 +0200

    Form add_budget corrected

--- budget_app/views.py
+++ budget_app/views.py
@@ -15,15 +15,12 @@ def register(request):
     return render(request, "budget_app/register.html")
 
 
+
 def add_budget(request):
-    return render(request, "budget_app/add_budget.html")
-
-
-def BudgetFormView(request):
     form = BudgetForm(request.POST or None)
     if form.is_valid():
         form.save()
     context = {
         'form': form
     }
-    return render(request, "add_budget.html", context)
+    return render(request, "budget_app/add_budget.html", context)
