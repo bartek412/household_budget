5aff9fdf66e3d319403f34a74d330c4437bca687
Maciej <maclis124@gmail.com>
2022-03-31 15:20:07 +0200

    Unified version

--- budget_app/urls.py
+++ budget_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.base, name='base'),
     path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('test_base/', views.test_base, name = 'test_base')
+    path('test_base/', views.test_base, name = 'test_base'),
+    path('add_budget/', views.add_budget, name='add_budget')
 ]
--- budget_app/views.py
+++ budget_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from os import path
+from .forms import BudgetForm
 
 base_path = path.join('budget_app', 'base.html')
 
@@ -17,3 +18,13 @@ def register(request, base_path = base_path):
 
 def test_base(request):
     return render(request, "budget_app/test_base.html")
+
+def add_budget(request, base_path = base_path):
+    if request.method == 'POST':
+        form = BudgetForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BudgetForm()
+  
+    return render(request, "budget_app/add_budget.html", {'form': form, 'base_path': base_path})
